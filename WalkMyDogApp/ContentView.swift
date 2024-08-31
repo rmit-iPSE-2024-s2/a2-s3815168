b6323702d91bdfe20e2bbfae8a73a0a69b13b35a
ArturUseinov <s3815168@student.rmit.edu.au>
2024-09-01 00:53:27 +1000

    Initial Commit

---
+++ WalkMyDogApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalkMyDogApp
+//
+//  Created by Artur on 1/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalkMyDogApp/WalkMyDogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkMyDogAppApp.swift
+//  WalkMyDogApp
+//
+//  Created by Artur on 1/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct WalkMyDogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
